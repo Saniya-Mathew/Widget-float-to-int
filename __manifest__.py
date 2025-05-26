d568a5540a147355c01e452490ab9d598e9ea848
saniya <saniyaelzabeth1543@gmail.com>
2025-05-26 12:56:44 +0530

    widget day 1

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': " Widget: Float-to-Integer",
+    'version': '1.0',
+    'category': '',
+    'type': 'module',
+    'depends': ['base'],
+    'description': "A widget that convert floatig values to nearest integer",
+    'data': [
+
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'float_to_int_widget/static/src/js/float_int.js',
+            'float_to_int_widget/static/src/xml/float_int.xml'
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+}
+
