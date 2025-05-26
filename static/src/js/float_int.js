a73cf9b3c8fba85a0f35a2ae4743e660bb69f10e
saniya <saniyaelzabeth1543@gmail.com>
2025-05-26 17:40:16 +0530

    widget day 1.1

--- static/src/js/float_int.js
+++ static/src/js/float_int.js
@@ -1,17 +1,32 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
 import { useInputField } from "@web/views/fields/input_field_hook";
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
-import { onRendered } from "@odoo/owl";
-import {Component, useState} from "@odoo/owl";
-import { useRef, useEffect, onWillRender, onMounted } from "@odoo/owl";
+import { Component, useRef, onWillRender, onMounted } from "@odoo/owl";
 import { parseFloat } from "@web/views/fields/parsers";
+
 export class FloatInt extends Component{
-   static template = "float_int_widget.FloatIntField";
+   static template = "float_int_widget";
    setup(){
-       this.input = useRef('inputfloatint')
+       this.options = [
+                     { label: 'UP', value: 'up' },
+                     { label: 'DOWN', value: 'down' },
+                     { label: 'HALF-UP', value: 'half_up' },
+                 ];
+       if (this.options == 'up')
+       {
+            this.rounded_up()
+       }
+        if (this.options == 'down')
+       {
+            this.rounded_down()
+       }
+        if (this.options == 'half_up')
+       {
+            this.rounded()
+       }
+       this.input = useRef('input_float_int')
        useInputField({ getValue: () => this.props.record.data[this.props.name] || "" ,
-       refName: "inputfloatint",parse: (v) => parseFloat(v),});
+       refName: "input_float_int",parse: (v) => parseFloat(v),});
        onWillRender(() =>  {
            this.rounded()
        });
@@ -25,7 +40,22 @@ export class FloatInt extends Component{
                this.props.record.data[this.props.name] = Math.round(this.input.el.value)
            }
    }
+
+    rounded_up(){
+       if (this.input.el)
+           {
+               this.props.record.data[this.props.name] = Math.ceil(this.input.el.value)
+           }
+   }
+
+     rounded_down(){
+       if (this.input.el)
+           {
+               this.props.record.data[this.props.name] = Math.floor(this.input.el.value)
+           }
+   }
 }
 FloatInt.component = FloatInt
 FloatInt.supportedTypes = ["float"]
 registry.category("fields").add("float_int_widget", FloatInt);
+
